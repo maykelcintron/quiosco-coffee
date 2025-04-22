f0872e67ec43bc604c61e1cf461b2b16a9c612a5
Maykel Cintron <tecnopcell211000@gmail.com>
2025-04-22 15:58:28 -0400

    Initial Commit

---
+++ app/order/layout.tsx
@@ -0,0 +1,18 @@
+import OrderSidebar from "@/components/OrderSidebar";
+import OrderSummary from "@/components/OrderSummary";
+
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+    return(
+        <>
+            <div className="md:flex">
+                <OrderSidebar />
+
+                <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
+                    {children}
+                </main>
+
+                <OrderSummary />
+            </div>
+        </>
+    )
+}
---
+++ components/OrderSidebar.tsx
@@ -0,0 +1,9 @@
+const OrderSidebar = () => {
+    return (
+        <aside className="w-72 md:h-screen bg-white">
+            OrderSidebar
+        </aside>
+    )
+}
+
+export default OrderSidebar
